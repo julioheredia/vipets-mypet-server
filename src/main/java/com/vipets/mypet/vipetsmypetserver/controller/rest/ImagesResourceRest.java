bcfeabfff97b2443f54484a3156af0db3a8abd55
julioheredia <jul.heredia@gmail.com>
2018-11-12 17:57:23 -0200

    refactory

--- src/main/java/com/vipets/mypet/vipetsmypetserver/controller/rest/ImagesResourceRest.java
+++ src/main/java/com/vipets/mypet/vipetsmypetserver/controller/rest/ImagesResourceRest.java
@@ -6,9 +6,7 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.servlet.ServletContext;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,18 +14,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vipets.mypet.vipetsmypetserver.util.ImagesUtil;
 import com.vipets.mypet.vipetsmypetserver.util.ImagesUtil.ImageType;
 
 @RestController
 public class ImagesResourceRest {
 
-	@Autowired
-	private ServletContext context;
-
 	@RequestMapping(value = "/images/{imageName}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
 	public @ResponseBody byte[] getImage(@PathVariable String imageName) throws IOException {
-		String diretorio = context.getRealPath("WEB-INF/images/");
-		BufferedImage originalImage = ImageIO.read(new File(diretorio + imageName));
+		String directory = ImagesUtil.getImagesDirectory();
+		BufferedImage originalImage = ImageIO.read(new File(directory + imageName));
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		ImageIO.write(originalImage, ImageType.jpeg.name(), baos);
 		baos.flush();
